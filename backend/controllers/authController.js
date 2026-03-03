dd8a846bcdfedbfb6dae2480512ec9f1a67e133b
DarshanKS09 <darshanksgowda09@gmail.com>
2026-03-03 08:50:33 +0530

    changed some ui part

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -13,7 +13,7 @@ export const sendOtp = async (req, res) => {
     const { email } = req.body;
 
     if (!email) {
-      return res.status(400).json({ message: "Email required" });
+      return res.status(400).json({ message: "Email is required" });
     }
 
     const otp = generateOtp();
